1d2853b70f54aedbb83d4dbbb37a88d4650f0323
Marcus R. Waldman <marcus_waldman@gse.harvard.edu>
2019-01-07 12:45:32 -0500

    Make unix commpatible

--- compile lpa-mi-src.R
+++ compile lpa-mi-src.R
@@ -8,13 +8,16 @@ library(roxygen2)
 library(devtools)
 
 # #Acer-Laptop
-  #dropbox = "C:/Users/marcu/Dropbox"
+  dropbox = "C:/Users/marcu/Dropbox"
 # Big Bertha
   #dropbox="D:/Dropbox/Dropbox"
 # Queen Mary's Revenge
   #dropbox = "D:/Dropbox"
 # Gaby
-  dropbox = "D:/Dropbox" 
+  #dropbox = "D:/Dropbox" 
+# IQSS-RCE NoMachine
+  #dropbox = "~/nfs/home/M/mwaldman/diss-nomx/Dropbox"
+  
 
 src_dir= paste(dropbox,"/Dissertation/lpa-mi-src", sep = "")
 #create("lpa.mi.src")
@@ -28,5 +31,5 @@ install("lpa.mi.src", upgrade = FALSE)
 rm(list = ls())
 
 
-#install_github("marcus-waldman/lpa-mi-src/lpa.mi.src")
+install_github("marcus-waldman/lpa-mi-src/lpa.mi.src")
 #library("lpa.mi.src")
