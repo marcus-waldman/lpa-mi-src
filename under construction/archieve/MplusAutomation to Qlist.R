4d003f766a9cc28f2c4d10213225df07e8fd56d5
Marcus Waldman <marcus_waldman@gse.harvard.edu>
2019-12-07 12:36:32 -0500

    Update to include rest.

--- under construction/archieve/MplusAutomation to Qlist.R
+++
@@ -1,8 +0,0 @@
-
-
-library(lpa.mi.src)
-library("MplusAutomation")
-setwd("C:/Users/marcu/Dropbox/Dissertation/lpa-mi-src/under construction")
-params_df = readModels(filefilter = "k-3naive dfcom p1 z1 rep1", what = "parameters")$parameters$unstandardized
-
-Mplus2Qlist(params_df)
